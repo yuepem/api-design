b1d0e1cbd84190e31e38340805afb9a25fe44f5c
x_yue <103266340+yuepem@users.noreply.github.com>
2024-06-14 14:01:37 +0200

    commit two versions api v1 and v2

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 
+// import routes from different version folders
+const v1Router = require('./src/v1/routes');
+const v2Router = require('./src/v2/routes')
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 
-// For Testing purposes
-app.get('/', (req, res) => {
-    res.send("It's working!");
-})
+ //For Testing purposes
+// app.get('/', (req, res) => {
+//     res.send("It's working!");
+// })
+
+app.use('/api/v1', v1Router);
+app.use('/api/v2', v2Router);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
